17829b75bfc86f76470f0e28f2f54f916d6eafca
Christiano0407 <placebo0704@gmail.com>
2022-05-30 11:31:22 -0500

    add api-keys

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,8 @@
-//** === API REST === */
-const API_KEY = "c66eb9e2b42b5d1d179fff7ac34ce71f";
-const API_URL =
-  "https://api.themoviedb.org/3/movie/550?api_key=c66eb9e2b42b5d1d179fff7ac34ce71f";
-const API_TOKEN =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNjZlYjllMmI0MmI1ZDFkMTc5ZmZmN2FjMzRjZTcxZiIsInN1YiI6IjYxODZlNGYyYTMxM2I4MDA4ZjQ1Y2NkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._SyV7z_TCNXZRmYDiihDspxJTkXT8LFU5MueX4MHDsg";
+console.log("Hello World API REST");
 //** API  Edit */
+/* import { API_KEY } from "./secrets.js"; */
+//console.log(`Key: ${API_KEY}`);
+/* import { API_URL } from "./secrets.js"; */
+//console.log(`URL: ${API_URL}`);
 
 //** === Project API MOVIES === */
